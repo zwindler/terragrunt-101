082127c3bd4bc75725bf1b96ebaeec0aa1fa959a
Denis GERMAIN <dt.germain@gmail.com>
2022-03-12 15:14:03 +0100

    chore: add variables

---
+++ dept-datascience/team-A/product1/product1-dev-zwindler/variables.tf
@@ -0,0 +1,35 @@
+variable "billing_account" {
+    default = "010101-ABABAB-010101"
+}
+
+variable "folder_id" {
+    default = "folders/1234567890"
+}
+
+variable "project_name" {
+    default = "product1-dev-zwindler"
+}
+
+variable "location" {
+    default = "EU"
+}
+
+variable "region" {
+    default = "europe-west1"
+}
+
+variable "zone" {
+    default = "europe-west1-b"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "team" {
+    default = "team_a"
+}
+
+variable "owner" {
+    default = "zwindler"
+}
---
+++ shared-services/states-bucket/variables.tf
@@ -0,0 +1,35 @@
+variable "billing_account" {
+    default = "010101-ABABAB-010101"
+}
+
+variable "folder_id" {
+    default = "folders/1234567890"
+}
+
+variable "project_name" {
+    default = "states-bucket"
+}
+
+variable "location" {
+    default = "EU"
+}
+
+variable "region" {
+    default = "europe-west1"
+}
+
+variable "zone" {
+    default = "europe-west1-b"
+}
+
+variable "environment" {
+    default = "shared"
+}
+
+variable "team" {
+    default = "team_infra"
+}
+
+variable "owner" {
+    default = "admin"
+}
