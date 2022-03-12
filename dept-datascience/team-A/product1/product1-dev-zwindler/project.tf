a359236e4a21c40df53ec2b8352135daaab02410
Denis GERMAIN <dt.germain@gmail.com>
2022-03-12 14:13:31 +0100

    chore: switch to variables for billing and folder parent

--- dept-datascience/team-A/product1/product1-dev-zwindler/project.tf
+++ dept-datascience/team-A/product1/product1-dev-zwindler/project.tf
@@ -1,7 +1,7 @@
 module "project" {
   source          = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/project"
-  billing_account = "01AB34-CD56EF-78GH90"
-  name            = "product1-dev-zwindler"
-  parent          = "folders/123456789012"
+  billing_account = var.billing_account
+  name            = var.project_name
+  parent          = var.folder_id
   services        = []
 }
